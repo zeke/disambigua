9c93bbce221d10821888c0a46a3b644d6da1cc65
zeke <zeke@sikelianos.com>
2011-05-22 00:55:41 -0700

    Removed some debugging stuff:

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,8 +3,6 @@ class TermsController < ApplicationController
   def show
     @term = Term.find_or_initialize_by_name(params[:id])
     
-    Rails.logger.debug @term.disambiguation_url.red_on_yellow
-    
     @term.disambiguate! if @term.new_record?
 
     respond_to do |format|
