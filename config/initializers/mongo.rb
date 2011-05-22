ca85c3c50a6b8ca2cfcdd5cb0a566f281d8dafdf
zeke <zeke@sikelianos.com>
2011-05-22 00:08:14 -0700

    Updated mongo config for heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,1 +1,8 @@
-MongoMapper.database = "disambigua_#{Rails.env}"
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  MongoMapper.database = "disambigua_#{Rails.env}"
+  # MongoMapper.config = {RAILS_ENV => {'uri' => "mongodb://localhost/disambigua_#{Rails.env}"}}
+end
+
